5b8da9c6abed3604c8e43239632e491fa59af7e1
darrenak403 <ngothanhdat4002@gmail.com>
2025-11-15 13:23:33 +0700

    style(todo_tile): adjust padding for todo tile layout
    Changes:
    - Update padding to use specific values for left, right, and top
    - Remove unnecessary line in Checkbox widget

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -15,7 +15,7 @@ class ToDoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
       child: Container(
         padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
@@ -30,7 +30,6 @@ class ToDoTile extends StatelessWidget {
               onChanged: onChanged,
               activeColor: Colors.black,
             ),
-
             //task name
             Text(taskName),
           ],
